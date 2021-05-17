67d07f456f005a405976ec0382ed6bbea03c19da
Daniel Lins <dlins300@gmail.com>
2021-05-16 23:19:32 -0300

    .

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-var acc = document.getElementsByClassName("accordion");
+const acordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAcordion() {
+  this.classList.toggle('ativo');
+  this.nextElementSibling.classList.toggle('ativo');
+}
+
+acordionList.forEach((item) => {
+  item.addEventListener('click', activeAcordion);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+  
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
@@ -15,4 +39,4 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
-}
+}*/
